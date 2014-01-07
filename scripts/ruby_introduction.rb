1374450a9043642e0ef868cb3f406f3fcb88368e
Pito Salas <pitosalas@gmail.com>
2014-01-07 17:44:52 -0400

    lecture number support

--- scripts/ruby_introduction.rb
+++ scripts/ruby_introduction.rb
@@ -1,25 +1,37 @@
 	# Strings, Numbers are a class
 
-	1.class
-	"Greetings".class
+	1.class            # => Fixnum
+	"Greetings".class  # => String
 
 	# We use "new" to create new instances
 
-	a = Array.new(5)
-	a.size
+	a = Array.new(5)  # => [nil, nil, nil, nil, nil]
+	a.size            # => 5
 
-	a[0] = "Truth"
-	a[1] = "even"
-	a[2] = "unto"
-	a[3] = "its"
-	a[4] = "innermost"
-	a[5] = "parts"
+	a[0] = "Truth"      # => "Truth"
+	a[1] = "even"       # => "even"
+	a[2] = "unto"       # => "unto"
+	a[3] = "its"        # => "its"
+	a[4] = "innermost"  # => "innermost"
+	a[5] = "parts"      # => "parts"
 
-	a.size
-	a.class
+	a.size    # => 6
+	a.class   # => Array
+	a.sample  # => "innermost"
+	a.sort    # => ["Truth", "even", "innermost", "its", "parts", "unto"]
+	
 
-	puts a
-	puts a.inspect
-	puts a.join(" ")
+	puts a            # => nil
+	puts a.inspect    # => nil
+	puts a.join(" ")  # => nil
 
 
+
+# >> Truth
+# >> even
+# >> unto
+# >> its
+# >> innermost
+# >> parts
+# >> ["Truth", "even", "unto", "its", "innermost", "parts"]
+# >> Truth even unto its innermost parts
